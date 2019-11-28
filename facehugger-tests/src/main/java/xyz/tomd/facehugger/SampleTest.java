af7b11451903e22d2a389c822172ee52761fd9ca
Tom Donohue <monodot@gmail.com>
2019-11-28 21:18:46 +0100

    wip

---
+++ facehugger-tests/src/main/java/xyz/tomd/facehugger/SampleTest.java
@@ -0,0 +1,13 @@
+package xyz.tomd.facehugger;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleTest {
+
+    @Test
+    public void testSomething() throws Exception {
+        System.out.println("This will just generate a report of some kind");
+    }
+}
